02556efe3133d5e8412afb0c2777635428f3b030
Franco Polizzi <me@fpolizzi.com>
2026-03-01 22:25:15 +0100

    feat: add functional interface and lambda

--- src/main/java/com/fpolizzi/Main.java
+++ src/main/java/com/fpolizzi/Main.java
@@ -15,5 +15,24 @@ public class Main {
         // use another Email class that implements the same interface
         EmailSender emailSenderTwo = new Hotmail();
         emailSenderTwo.send();
+
+        // to be more concise, we can implement a functional interface
+        // which can only have one abstract method
+        // @FunctionalInterface optional annotation
+        /*EmailSender amigosCodeSender = new EmailSender() {
+            @Override
+            public void send() {
+                System.out.println("sending email using Amigoscode server...");
+            }
+        };*/
+
+        // implement the same functional interface using a lambda,
+        // the curly braces can be omitted
+        EmailSender amigosCodeSender = () -> {
+                System.out.println(
+                        "sending email using Amigoscode server...");
+        };
+
+        amigosCodeSender.send();
     }
 }
