6ad2925f30dbbc2768921b77ffeeb14ba5953345
Franco Polizzi <me@fpolizzi.com>
2026-03-15 22:32:01 +0100

    fix: remove unused args[]

--- src/main/java/com/fpolizzi/functional/BiPredicateMain.java
+++ src/main/java/com/fpolizzi/functional/BiPredicateMain.java
@@ -8,7 +8,7 @@ import java.util.function.Predicate;
  */
 public class BiPredicateMain {
 
-    static void main(String[] args) {
+    static void main() {
 
         var email = "hello@amigoscode.com";
 
