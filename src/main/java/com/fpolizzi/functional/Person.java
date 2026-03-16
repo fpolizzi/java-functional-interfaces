bee32f18972835dfd0938481b0c777850fd8cd0e
Franco Polizzi <me@fpolizzi.com>
2026-03-16 22:13:46 +0100

    feat: demonstrate the usage of the combinator pattern

---
+++ src/main/java/com/fpolizzi/functional/Person.java
@@ -0,0 +1,10 @@
+package com.fpolizzi.functional;
+
+/**
+ * Created by fpolizzi on 16.03.26
+ */
+public record Person(
+        String name,
+        String phoneNumber,
+        String email) {
+}
---
+++ src/main/java/com/fpolizzi/functional/PersonRegistrationValidatorMain.java
@@ -0,0 +1,31 @@
+package com.fpolizzi.functional;
+
+/**
+ * Created by fpolizzi on 16.03.26
+ */
+public class PersonRegistrationValidatorMain {
+
+    static void main() {
+
+        Person jamila = new Person(
+                "Jamila",
+                "+0000000",
+                "jamila@gmail.com"
+        );
+
+        Person alex = new Person(
+                "Alex",
+                "123456",
+                "alex@gmail.com"
+        );
+
+        PersonRegistrationValidator validator =
+                PersonRegistrationValidator
+                        .isEmailTaken()
+                        .and(PersonRegistrationValidator.isEmailValid())
+                        .and(PersonRegistrationValidator.isPhoneNumberValid());
+
+        System.out.println(validator.apply(jamila));
+        System.out.println(validator.apply(alex));
+    }
+}
