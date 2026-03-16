b2e70159378d409e80ba77beabfe588342f85a97
Franco Polizzi <me@fpolizzi.com>
2026-03-16 23:35:37 +0100

    feat: implement a callback in java with the help of Consumer

---
+++ src/main/java/com/fpolizzi/functional/Callback.java
@@ -0,0 +1,31 @@
+package com.fpolizzi.functional;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by fpolizzi on 16.03.26
+ */
+public class Callback {
+
+    static void main() {
+
+        hello("Alex", null, cb);
+    }
+
+    static Consumer<String> cb = input ->
+            System.out.println(input + " you must have a last name");
+
+    static void hello(String firstName,
+                      String lastName,
+                      Consumer<String> callback) {
+
+        System.out.println(firstName);
+
+        if (lastName != null) {
+            System.out.println(lastName);
+        } else {
+            callback.accept(firstName);
+        }
+    }
+
+}
